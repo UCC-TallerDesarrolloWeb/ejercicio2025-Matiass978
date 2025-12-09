e868d3bed7265083a67da2b31f9f235492ac91d2
DemetrioRufeil <demetriorufeil@gmail.com>
2025-12-09 15:26:25 -0300

    ejercicios documentacion

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -1,9 +1,9 @@
 /**
- * Descripción
- * @method Nombre de la función
- * @param Parámetro A
- * @param Parámetro B
- * @return Valor que retorna
+ * conversion de unidades de metros, yardas, pies y pulgadas
+ * @method cambiarunidades
+ * @param {string} id - El id de los inputs de metros, yardas, pies o pulgadas
+ * @param {number} valor - el valor de los inputs de metros, yardas, pies o pulgadas
+ * @return 
  */
 
 function cambiarunidades(id, value) {
@@ -21,5 +21,9 @@ function cambiarunidades(id, value) {
         document.lasunidades.unid_pulgada.value = 0.0254*value;
         document.lasunidades.unid_pie.value = 0.083333*value;
         document.lasunidades.unid_yarda.value = 0.0277778*value;
+    }else if(id=="pie") {
+        document.lasunidades.unid_pulgada.value = 0.3048*value;
+        document.lasunidades.unid_pie.value = 12*value;
+        document.lasunidades.unid_yarda.value = 0.33333*value;
     }
 }
