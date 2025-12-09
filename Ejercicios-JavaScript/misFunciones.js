1414fffbd9ff7833de91fb319192922ee958b3ab
DemetrioRufeil <demetriorufeil@gmail.com>
2025-12-09 13:37:36 -0300

    ejercicio conversor de unidades js

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,21 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+function cambiarunidades(id, value) {
+    if(isNaN(value)){
+        alert("se ingreso un valor invalido");
+        document.lasunidades.unid_metro.value = "";
+        document.lasunidades.unid_pulgada.value = "";
+        document.lasunidades.unid_pie.value = "";
+        document.lasunidades.unid_yarda.value = "";
+    }else if(id=="metro") {
+        document.lasunidades.unid_pulgada.value = 39.3701*value;
+        document.lasunidades.unid_pie.value = 3.28084*value;
+        document.lasunidades.unid_yarda.value = 1.09361*value;
+    }else if(id=="pulgada") {
+        document.lasunidades.unid_pulgada.value = 0.0254*value;
+        document.lasunidades.unid_pie.value = 0.083333*value;
+        document.lasunidades.unid_yarda.value = 0.0277778*value;
+    }
+}
