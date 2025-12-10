3bc67266d7c7c608140d2a23a82a299471d12426
DemetrioRufeil <demetriorufeil@gmail.com>
2025-12-10 12:35:54 -0300

    ejercicio producto

---
+++ Ejercicios-JavaScript/funciones.js
@@ -0,0 +1,7 @@
+let mostrarDetalle = () => {
+  document.getElementById("detalle").style.display = "block";
+};
+
+let cerrarDetalle = () => {
+  document.getElementById("detalle").style.display = "none";
+};
